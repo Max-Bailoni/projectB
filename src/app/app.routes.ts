5e0093cc5c9d61985f313ac3142a620eeaaa1737
evao22 <eogunbor93@gmail.com>
2025-04-24 01:11:37 +0200

    added path: footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { AlumniComponent } from './alumni/alumni.component';
 import { CareersComponent } from './careers/careers.component';
 import { HomeComponent } from './home/home.component';
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
 
     { path: "", component: HomeComponent },
     { path: "alumni/:id", component: AlumniComponent },
-    { path: "careers", component: CareersComponent }
-
+    { path: "careers", component: CareersComponent },
+    {path: "footer", component: FooterComponent},
 ];
