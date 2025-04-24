bab4e61741972c115b814645217b49c380093e45
andreakaufmann <kaufmann_andrea@outlook.com>
2025-04-24 13:36:52 +0200

    Update events.component.ts

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { events } from '../Events';
+import { EventModel } from '../events.interface';
 
 @Component({
   selector: 'app-events',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
 export class EventsComponent {
+  events: EventModel[] = events;
 
 }
