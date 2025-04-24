3a37dda24de67e0fae4c5d1f9dfec72cd29a7520
andreakaufmann <kaufmann_andrea@outlook.com>
2025-04-24 13:33:13 +0200

    Update alumni.component.ts

--- src/app/alumni/alumni.component.ts
+++ src/app/alumni/alumni.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-import { Ialumni } from '../alumni.interface';
-import { ALUMNIS } from '../alumni';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Alumni } from '../alumni.interface';
+import { alumnis } from '../alumni';
 
 @Component({
   selector: 'app-alumni',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './alumni.component.html',
   styleUrl: './alumni.component.css'
 })
 export class AlumniComponent {
-  Alumni: Ialumni[] = ALUMNIS;
-  person:Ialumni | undefined = {} as Ialumni;
-  id:number = 0;
+  alumnis: Alumni[] = alumnis;
 
-  constructor(private route:ActivatedRoute){
-    let id = Number(this.route.snapshot.params['id']);
-    this.person = ALUMNIS.find(val => val.id == id);
-  }
 }
